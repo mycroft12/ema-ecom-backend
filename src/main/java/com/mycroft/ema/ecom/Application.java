458339a38de249f57549203ebdd6bc14e44d7911
mycroft <doughmi.fath.eddine@gmail.com>
2025-10-06 17:35:55 +0100

    first commit

---
+++ src/main/java/com/mycroft/ema/ecom/Application.java
@@ -0,0 +1,13 @@
+package com.mycroft.ema.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
